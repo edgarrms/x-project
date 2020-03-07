b2adcbe1c88b2a75f25580efcd1ab5bb8faddf87
Tulus Tiranda <tulustiranda@gmail.com>
2020-03-07 13:17:09 -0800

    added navbar, form, jumbotron and user input

---
+++ x-project/src/components/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../card/index"
+import Jumbotron from "../jumbotron"
+
+export default function Home(){
+    return(
+        <div>
+        <Jumbotron/>
+
+        <Card userName="example user # 1"/></div> 
+    )
+}
