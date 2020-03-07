7fbadfa91a96f1055197afaf1fc631f4819a86de
simonvlamoureux <50042252+simonvlamoureux@users.noreply.github.com>
2020-03-07 10:32:27 -0800

    Update index.js

--- components/CardTitleText/index.js
+++ components/CardTitleText/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import CardContext from "../../utils/CardContext";
 
 function CardTitleText() {
-  return <CardContext.Consumer>{({ title }) => <h2>{title}</h2>}</CardContext.Consumer>;
+  return <CardContext.Consumer>{({ Fuck My EX }) => <h2>{Fuck My EX}</h2>}</CardContext.Consumer>;
 }
 
 export default CardTitleText;
