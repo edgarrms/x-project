0978d29624d62517bac8ec6744037084c89e398f
Edgar <ramosjair94@gmail.com>
2020-03-07 13:28:00 -0800

    working on sign up sign in

--- x-project/src/App.js
+++ x-project/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="App__Aside"></div>
+
+            <div className="App__Form">
+
+                <div className="PageSwitcher">
+
+                    <a href="#" className="PageSwitcher__Item">Sign In</a>
+
+                    <a href="#" className="PageWitcher__Item PageWitcher__Item--Active">Sign Up</a>
+
+                </div>
+
+                <div className="FormTittle">
+                    <a href="#" className="FormTittle__Link">Sign In</a> or <a href="#"
+                        className="FormTittle__Link FormTittle__Link--Active">Sign Up</a>
+                </div>
+
+                <div className="FormCenter">
+                    <form className="FormFields" onSubmit={this.handleSubmit}>
+                        <div className="FormField">
+                            <label className="FormField__Label" htmlFor="name">Full Name</label>
+                            <input type="text" id="name" className="FormField__input" placeholder="Enter your full namwe" name="name" />
+                        </div>
+                        <div className="FormField">
+                            <label className="FormField__Label" htmlFor="password">Password</label>
+                            <input type="password" id="password" className="FormField__input" placeholder="Enter your full password" name="password" />
+                        </div>
+                        <div className="FormField">
+                            <label className="FormField__Label" htmlFor="email">Email</label>
+                            <input type="email" id="email" className="FormField__input" placeholder="Enter your Email" name="password" />
+                        </div>
+                    </form>
+                </div>
+
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
