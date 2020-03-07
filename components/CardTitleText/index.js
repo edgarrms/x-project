f3134736f2b2065da82206a645d42dbb0223cc72
simonvlamoureux <50042252+simonvlamoureux@users.noreply.github.com>
2020-03-07 13:22:14 -0800

    Delete index.js

--- components/CardTitleText/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CardContext from "../../utils/CardContext";
-
-function CardTitleText() {
-  return <CardContext.Consumer>{({ Fuck My EX }) => <h2>{Fuck My EX}</h2>}</CardContext.Consumer>;
-}
-
-export default CardTitleText;
